7cf1c470b62a025abb2073a67f446f7439354b87
lpenzey <lucaspenzeymoog@gmail.com>
2018-06-13 11:24:11 -0500

    deleted redundant tests

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -13,31 +13,3 @@ class Coinchanger
 		coin_array
 	end			
 end
-	# def dispense(change)
-	# 	penny = 1
-	# 	nickel = 5
-	# 	dime = 10
-	# 	quarter = 25
-	# 	coin_array = []
-	
-	# 	while change > 24
-	# 		change -=25
-	# 		coin_array << quarter
-	# 	end	
-
-	# 	while change > 9
-	# 		change -=10
-	# 		coin_array << dime
-	# 	end	
-		
-	# 	while change > 4
-	# 		change -= 5
-	# 		coin_array << nickel
-	# 	end
-
-	# 	change.times do |i|
-	# 		coin_array << penny
-	# 	end
-	# 	coin_array
-	# end
-#end
