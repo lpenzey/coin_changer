0c8923b6f84823c5b3f1976f532b11e2b29da153
lpenzey <lucaspenzeymoog@gmail.com>
2018-06-13 11:22:08 -0500

    deleted redundant tests

--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -17,28 +17,12 @@ RSpec.describe Coinchanger, "dispense" do
 		expect(coin_changer.dispense(2)).to eq [penny, penny]  
 	end
 
-	it "given three cents it returns three pennies" do 
-		expect(coin_changer.dispense(3)).to eq [penny, penny, penny]  
-	end
-
 	it "given five cents it returns one nickel" do 
 		expect(coin_changer.dispense(5)).to eq [nickel]  
 	end
 
 	it "given six cents it returns one nickel and one penny" do 
 		expect(coin_changer.dispense(6)).to eq [nickel, penny]  
-	end
-	
-	it "given seven cents it returns one nickel and two pennies" do 
-		expect(coin_changer.dispense(7)).to eq [nickel, penny, penny]  
-	end
-
-	it "given eight cents it returns one nickel and three pennies" do 
-		expect(coin_changer.dispense(8)).to eq [nickel, penny, penny, penny]  
-	end
-
-	it "given nine cents it returns one nickel and four pennies" do 
-		expect(coin_changer.dispense(9)).to eq [nickel, penny, penny, penny, penny]  
 	end
 
 	it "given 10 cents it returns one dime" do 
@@ -49,40 +33,12 @@ RSpec.describe Coinchanger, "dispense" do
 		expect(coin_changer.dispense(11)).to eq [dime, penny]  
 	end
 
-	it "given 11 cents it returns one dime and one penny" do 
-		expect(coin_changer.dispense(11)).to eq [dime, penny]  
-	end
-
-	it "given 12 cents it returns one dime and two pennies" do 
-		expect(coin_changer.dispense(12)).to eq [dime, penny, penny]  
-	end
-
-	it "given 13 cents it returns one dime and three pennies" do 
-		expect(coin_changer.dispense(13)).to eq [dime, penny, penny, penny]  
-	end
-
-	it "given 14 cents it returns one dime and four pennies" do 
-		expect(coin_changer.dispense(14)).to eq [dime, penny, penny, penny, penny]  
-	end
-
 	it "given 15 cents it returns one dime and one nickel" do 
 		expect(coin_changer.dispense(15)).to eq [dime, nickel]  
 	end
 
 	it "given 16 cents it returns one dime and one nickel and one penny" do 
 		expect(coin_changer.dispense(16)).to eq [dime, nickel, penny]  
-	end
-
-	it "given 17 cents it returns one dime and one nickel and two pennies" do 
-		expect(coin_changer.dispense(17)).to eq [dime, nickel, penny, penny]  
-	end
-
-	it "given 18 cents it returns one dime and one nickel and two pennies1" do 
-		expect(coin_changer.dispense(18)).to eq [dime, nickel, penny, penny, penny]  
-	end
-
-	it "given 19 cents it returns one dime and one nickel and four pennies" do 
-		expect(coin_changer.dispense(19)).to eq [dime, nickel, penny, penny, penny, penny]  
 	end
 
 	it "given 20 cents it returns two dimes" do 
